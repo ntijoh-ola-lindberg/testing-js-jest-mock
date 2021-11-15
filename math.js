515f1a6392aaa6e552109110c1c9b3992694f40c
Ola Lindberg <ola.lindberg@ntig.se>
2021-11-15 15:19:04 +0100

    Creating Math class + tests

---
+++ math.js
@@ -0,0 +1,12 @@
+class Math {
+
+  sum(a, b) {
+    return a + b;
+  }
+
+  sub(a, b) {
+    return a - b;
+  }
+
+}
+module.exports = Math;
---
+++ math.test.js
@@ -0,0 +1,14 @@
+const Math = require('../math');
+var math = new Math();
+
+test('Addition: 1 + 2 to equal 3', () => {
+  expect(math.sum(1, 2)).toBe(3);
+});
+
+test('Subtraction: 2 - 1 to equal 1', () => {
+  expect(math.sub(2, 1)).toBe(1);
+});
+
+test('Subtraction: 1 - 2 to equal -1', () => {
+  expect(math.sub(1, 2)).toBe(-1);
+});
